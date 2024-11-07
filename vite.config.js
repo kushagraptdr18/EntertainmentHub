0d05b379fd1edb134aa1d1b8f584aae260fce784
kushagra patidar <kushagraptdr18@gmail.com>
2024-11-07 10:12:27 +0530

    some changes related to chunk size

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000 // Set the limit in KB (default is 500 KB)
+}
 })
+
